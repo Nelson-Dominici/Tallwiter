02ea308dc80f669b79b829f4eb82bc8dfdd42031
Nelson-Dominici <nelsoncomer777@gmail.com>
2024-07-05 15:22:15 -0300

    feat: add tallstackui config

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./resources/**/*.blade.php",
+      './vendor/tallstackui/tallstackui/src/**/*.php'
+    ],
+
+    presets: [
+        require('./vendor/tallstackui/tallstackui/tailwind.config.js')
+    ],
+
+    theme: {
+        screens: {
+            phone: "462px",
+            ...defaultTheme.screens
+        },
+        extend: {
+            colors: {
+                primary: '#51B9F5',
+                secondary: '#E66AA9',
+                commonTextColor: '#111827'
+            },
+            fontFamily: {
+            'questrial': 'Questrial',
+            }
+        },
+    },
+    plugins: [forms],
+}
