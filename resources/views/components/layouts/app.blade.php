da4a147c12fcbaa99a90d3eec4b1e1318c94fb55
Nelson-Dominici <nelsoncomer777@gmail.com>
2024-07-07 12:56:25 -0300

    feat: add tallstack script

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,12 +3,13 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        @vite('resources/css/tailwind.css')
         <link href="https://fonts.googleapis.com/css2?family=Questrial&display=swap" rel="stylesheet">
-        <title>TALLBlog - {{ $title ?? 'Page Title' }}</title>
-        <link rel="icon" href="{{asset('/images/fiveicon.png')}}" type="image/x-icon">
+        <title>{{ $title ?? 'Page Title' }} / Tallwiter</title>
+        <link rel="icon" href="{{asset('/images/favicon-logo.png')}}" type="image/x-icon">
+        <tallstackui:script />
+        @vite('resources/css/tailwind.css')
     </head>
-    <body class="font-questrial h-screen">
+    <body class="font-questrial h-screen bg-red">
         {{ $slot }}
     </body>
 </html>
