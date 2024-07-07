f162f1c1910313b8402ad4cb0b2bfa923a2c61a0
Nelson-Dominici <nelsoncomer777@gmail.com>
2024-07-07 12:55:44 -0300

    feat: add create user view

--- resources/views/livewire/users/create.blade.php
+++ resources/views/livewire/users/create.blade.php
@@ -1,58 +1,29 @@
-<div class='flex h-screen'>
+<div class='flex w-full h-full items-center justify-center'>
 
-    <section class='hidden lg:flex items-center flex-col justify-center w-full h-full bg-primary'>
+    <form wire:submit='save' class='px-6 flex flex-col items-center justify-center phone:w-96 w-full'>
 
-        <div class='flex items-center flex-col justify-center'>
+        <img class="w-44 mb-1" src="{{asset('/images/logo.png')}}">
 
-            <img class="w-1/2 object-cover" src="{{asset('/images/logo.png')}}" >
-
-            <p class="text-white mt-3 text-lg font-semibold w-10/12 text-center">
-                A community-powered open source blog updated with the latest news about TALLStack, offering insights and updates on new features and best practices.
-            </p>
-
+        <div class='w-full mb-4'>
+            <x-input required label='Name *' hint='Insert a name' wire:model='name' class='h-10'/>
         </div>
 
-        <a class='flex items-center justify-center text-white border-2 w-60 text-center rounded-full mt-8 p-3 font-bold' target='_blank' href='https://github.com/Nelson-Dominici/TALLBlog'>
-            <x-bi-github class='mr-2'/>
-            GitHub
-        </a>
+        <div class='w-full mb-4'>
+            <x-input required label='Email *' hint='Insert a valid email' wire:model='email' class='h-10'/>
+        </div>
 
-    </section>
+        <div class='w-full mb-4'>
+            <x-input required label='Password *' hint='Choose a password with at least 6 characters' wire:model='password' class='h-10'/>
+        </div>
 
-    <section class='flex items-center bg-background justify-center w-full h-full'>
+        <x-button type='submit' class='focus:!ring-0 text-white font-semibold text-xl phone:w-60 w-full p-3 bg-primary' wire:loading.class='opacity-75' round>
+            Create
+        </x-button>
 
-        <form wire:submit='save' class='px-6 flex flex-col items-center justify-center w-full'>
+        <p class='mt-6 font-semibold text-commonTextColor'>
+            Do you already have an <a wire:navigate class='text-primary' href='{{ route("auth.login")}}'>account</a> ?
+        </p>
 
-            <h1 class="
-                mb-3 bg-secondary text-white font-semibold rounded-lg p-1 text-center phone:w-96 w-full phone:h-8 h-auto transition-opacity
-                {{ $errors->any() ? 'opacity-100' : 'opacity-0' }}"
-            >
-                @if($errors->has('name'))
-                    {{ $errors->first('name') }}
-                @elseif($errors->has('email'))
-                    {{ $errors->first('email') }}
-                @elseif($errors->has('password'))
-                    {{ $errors->first('password') }}
-                @endif
-            </h1>
-
-
-            <h1 class='font-semibold mb-6 text-primary phone:text-4xl text-2xl'>🤙Create Account</h1>
-
-            <input wire:model='name' id='name' required placeholder='Name' class='outline-0 p-3 border rounded-md mb-5 phone:w-96 w-full' type='text' name='name'>
-
-            <input wire:model='email' id='email' required placeholder='Email' class='outline-0 p-3 border rounded-md mb-5 phone:w-96 w-full' type='email' name='email'>
-
-            <input wire:model='password' id='password' required placeholder='Password' class='outline-0 p-3 border rounded-md mb-6 phone:w-96 w-full' type='password' name='password'>
-
-            <input wire:loading.class="opacity-75" type='submit' class='transition-all text-white font-semibold text-xl cursor-pointer phone:w-60 w-full rounded-full p-3 bg-primary' value='Create'>
-
-            <p class="mt-6 font-semibold text-commonTextColor">
-                Do you already have an <a wire:navigate class="text-primary" href='{{ route("auth.login")}}'>account</a> ?
-            </p>
-
-        </form>
-
-    </section>
+    </form>
 
 </div>
