41a2a26ce2f1a7bcb9bd93e7e8a66dbe31035d12
Nelson-Dominici <nelsoncomer777@gmail.com>
2024-07-12 23:29:01 -0300

    feat: add intersect alpine plugin

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,10 @@
 import axios from 'axios';
+
+import Alpine from 'alpinejs'
+import intersect from '@alpinejs/intersect'
+
+Alpine.plugin(intersect);
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
