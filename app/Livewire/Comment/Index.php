1251adfe687a208a889859619ae10731d4035c8b
Nelson-Dominici <nelsoncomer777@gmail.com>
2024-08-21 21:13:49 -0300

    feat: add addComment metho

--- app/Livewire/Comment/Index.php
+++ app/Livewire/Comment/Index.php
@@ -2,7 +2,8 @@
 
 namespace App\Livewire\Comment;
 
-use Livewire\Attributes\Validate;
+use Illuminate\Support\Facades\DB;
+use Livewire\Attributes\On;
 use Illuminate\View\View;
 use App\Models\Comment;
 use Livewire\Component;
@@ -10,28 +11,35 @@ use App\Models\Post;
 
 class Index extends Component
 {
-    #[Validate('bail|required|string|min:3|max:255')]
-    public string $text;
-
     public Post $post;
+    public array $comments;
 
-    public function create(): void
+    #[On('comment-created.{post.id}')]
+    public function addComment($comment)
     {
-        $body = [
-            'post_id' => $this->post->id,
-            'user_id' => auth()->user()->id,
-            'text' => $this->validate()['text']
-        ];
+        $comment['liked'] = 0;
+        $comment['likes_count'] = 0;
 
-        Comment::create($body);
+        $this->comments[] = $comment;
+    }
 
-        $this->reset(['text']);
+    public function mount(): void
+    {
+        $userId = auth()->user()->id;
+
+        $query = Comment::where('comments.post_id', $this->post->id)
+            ->leftJoin('comments_likes', 'comments.id', '=', 'comments_likes.comment_id')
+            ->select('comments.*',
+                    DB::raw("COUNT(IF(comments_likes.user_id = {$userId}, comments_likes.comment_id, NULL)) AS liked"),
+                    DB::raw("COUNT(comments_likes.comment_id) AS likes_count"))
+            ->groupBy('comments.id')
+            ->orderBy('comments.created_at', 'asc');
+
+        $this->comments = $query->get()->toArray();
     }
 
     public function render(): View
     {
-        $comments = Comment::all()->where('post_id','=', $this->post->id);
-
-        return view('livewire.comment.index', compact('comments'));
+        return view('livewire.comment.index');
     }
 }
