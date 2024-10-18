944a2710ad4b34b196c6c97c867272a94237564f
Nelson-Dominici <nelsoncomer777@gmail.com>
2024-10-18 09:33:58 -0300

    feat: add profile image in the post

--- resources/views/livewire/components/post.blade.php
+++ resources/views/livewire/components/post.blade.php
@@ -1,20 +1,24 @@
 <div wire:loading.class='opacity-40 bg-gray-200' wire:target='delete()' class='transition-all w-full p-5 border-b flex' x-data='{
     openComments: false,
 }'>
-    <div class='!w-10 !h-10 bg-primary mt-1 rounded-full flex items-center justify-center mr-4'>
+    <div class='!size-11 bg-primary mt-1 rounded-full flex items-center justify-center mr-4 overflow-hidden'>
 
-        <p class='text-white font-bold text-lg'>
-            {{
-                strtoupper( substr($post->user->name, 0, 1) )
-            }}
-        </p>
+        @if(!$post->user->profile_photo_id)
+            <p class='text-white font-bold text-lg'>
+                {{
+                    strtoupper( substr($post->user->name, 0, 1) )
+                }}
+            </p>
+        @else
+            <x-cld-image public-id="{{$post->user->profile_photo_id}}" width="300" height="300" crop="scale"></x-cld-image>
+        @endif
 
     </div>
 
     <div class='grow w-min'>
 
         <div class='flex justify-between'>
-            <h3 class='font-semibold text-secondary'>{{$post->user->name}}</h3>
+            <a wire:navigate href="{{ '/users/profile/' . $post->user->id }}" class='font-semibold text-secondary'>{{$post->user->name}}</a>
 
             @if ($post->user_id == auth()->user()->id)
                 <x-dropdown icon="ellipsis-vertical" static>
