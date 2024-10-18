21192d48dc96f79f1beef47ee640e0211e089a3b
Nelson-Dominici <nelsoncomer777@gmail.com>
2024-10-18 09:57:13 -0300

    chore: add responsiveness to the component

--- resources/views/livewire/components/menu.blade.php
+++ resources/views/livewire/components/menu.blade.php
@@ -1,4 +1,4 @@
-<div class="w-80 pt-3 pl-10 border-r">
+<div class="md:w-80 pt-3 md:pl-10 border-r px-10 flex justify-center md:justify-normal">
 
     <div class='fixed'>
 
@@ -14,25 +14,25 @@
                 <x-heroicon-o-home class="w-8 text-secondary"/>
             @endif
 
-            <p class="ml-5 text-xl text-secondary">Home</p>
+            <p class="md:block hidden ml-5 text-xl text-secondary">Home</p>
 
         </a>
 
         <a class="flex items-center hover:bg-[#EEEEEE] rounded-full transition-all w-fit p-2 mt-2 pr-3" wire:navigate href="{{ route('home', ['filter' => 'for-you']) }}">
             <x-heroicon-o-bell class="w-8 text-secondary"/>
 
-            <p class="ml-5 text-xl text-secondary">Notifications</p>
+            <p class="md:block hidden ml-5 text-xl text-secondary">Notifications</p>
         </a>
 
-        <a class="flex items-center hover:bg-[#EEEEEE] rounded-full transition-all w-fit p-2 mt-2 pr-3" wire:navigate href='/users/edit'>
+        <a class="flex items-center hover:bg-[#EEEEEE] rounded-full transition-all w-fit p-2 mt-2 pr-3" wire:navigate href="{{ '/users/profile/' . auth()->user()->id }}">
 
-            @if ($selectButton == 'users/edit')
+            @if (Str::is('users/profile/*', $selectButton))
                 <x-heroicon-c-user class='w-8 text-secondary'/>
             @else
                 <x-heroicon-o-user class='w-8 text-secondary'/>
             @endif
 
-            <p class="ml-5 text-xl text-secondary">Profile</p>
+            <p class="md:block hidden ml-5 text-xl text-secondary">Profile</p>
         </a>
 
     </div>
