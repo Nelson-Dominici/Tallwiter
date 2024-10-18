7c90c8555ea880283d7c574d0e5edf899ae78445
Nelson-Dominici <nelsoncomer777@gmail.com>
2024-10-18 09:55:27 -0300

    feat: add new user infos

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'description',
+        'banner_photo_id',
+        'profile_photo_id'
     ];
 
     /**
