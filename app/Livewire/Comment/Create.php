b629d75f15344019285e42a3e1b2b76d98897c4d
Nelson-Dominici <nelsoncomer777@gmail.com>
2024-10-20 20:53:04 -0300

    chore: notify post writer

--- app/Livewire/Comment/Create.php
+++ app/Livewire/Comment/Create.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Comment;
 
 use App\Models\Comment;
+use App\Models\Notification;
 use App\Models\Post;
 use Livewire\Component;
 use Livewire\Attributes\Validate;
@@ -26,6 +27,19 @@ class Create extends Component
         ];
 
         $comment = Comment::create($body);
+
+        if ($this->post->user_id !== auth()->user()->id) {
+
+            Notification::create([
+                'type' => 'comment',
+                'image' => auth()->user()->profile_photo_id,
+                'sender_id' => auth()->user()->id,
+                'user_id' => $this->post->user_id,
+                'text' => $this->validate()['text'],
+                'title' => auth()->user()->name.' commented on your post',
+            ]);
+
+        }
 
         $this->reset(['text']);
 
